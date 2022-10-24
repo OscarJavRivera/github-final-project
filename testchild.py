5f133a8795fa4638dfa132165ba928f05c1ae749
Oscar Rivera <ing.oscar.rivera@gmail.com>
2022-10-24 20:02:37 +0200

    Committed

--- testchild.py
+++ testchild.py
@@ -1,2 +1,48 @@
 ## Adding a new file in child branch
+from operator import truediv
+
+
 print("Lord of the Rings is ’the best ’ fantasy movie ever !")
+print ("Hello, World!!!!")
+print("Hola \n \t \t que tal??")
+print("\n Creating Random Variables")
+
+random_variable_1 = 5
+random_variable_2 = 7
+print(random_variable_1 == random_variable_2)
+
+# In the yellow box below, try getting accustomed to the mathematical operators by doing the following tasks.
+# - [x] Add 2 and 3 using the + operator.
+print(2+3)
+# - [ ] Substract 10 from 3 using the - operator.
+print(10-3)
+# - [ ] Multiply 5 times 6 using the * operator.
+print(5*6)
+# - [ ] Divide 1 by 2 using the / operator.
+print(1/2)
+# - [ ] Divide 3 by 2 using the // (integer division) operator. (Addon: Can you figure out the difference between / and // operator?).
+print(3//2)
+# - [ ] Get the remainder by after dividing 11 by 5 using the % operator.
+print(11%5)
+# - [ ] Raise 2 to the power of 3 by using the ** operator.
+print(2**3)
+# - [x] Bonus question: Try taking two numbers as inputs and store 
+# them in 2 variables and then add them and print the result.
+number1=1
+number2=2
+result=number1+number2
+print("Result of addition", result)
+# - [ ] Do the following operation: 2 ** 3 + 5 - 2. Can you figure out which operation was done first and which was done last?
+#---->2 to the power of tree, is carried out first
+# - [ ] As a disclaimer, try defining two string (alphabet) variables, like, string_1 = ’Hello’ and string_2 = ’World’ and try using the + , - , * operators on them. Check what happens!
+string1='hello'
+string2='World'
+print(string1 + "---"+ string2)
+
+# Try out yourself some of the ways of using logical operators.
+# - [ ] Set a variable, x to True, then print out the not(x).
+x = False
+print(not(x))
+# - [ ] Create another variable y and set it to False. Print the value of x and y.
+# - [ ] Using the same variables print the value of x or y.
+# - [ ] Bonus Question: Try using all the three logical operators in a equation and try to figure out which operator works first and which one operates last.
