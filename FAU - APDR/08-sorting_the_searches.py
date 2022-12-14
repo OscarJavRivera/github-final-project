6b9d22794d5c04f49e0900f495eb955c06762f5b
Oscar Rivera <ing.oscar.rivera@gmail.com>
2022-12-14 21:48:06 +0100

    Ex 08 - Test Cases are running but unsuccessful

---
+++ FAU - APDR/08-sorting_the_searches.py
@@ -0,0 +1,16 @@
+class ModifiedMergeSort:
+    
+	def __init__(self,data):
+		self.data=data
+    
+	def sorting(self):
+		print(self.data)
+
+
+## Create your BinarySearch class here ##
+class BinarySearch:
+	def __init__(self,data):
+		self.data=data
+
+	def search(self,data):
+		self.data=data
